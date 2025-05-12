d0a33def42085cbf33bc59d0b5886d8ad794d6e9
𝓝𝓪𝓱𝓲𝓭 ☠️ <alimnh412@gmail.com>
2025-05-12 11:08:10 +0600

    wfgwfkwfe

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,10 +22,10 @@ const index = () => {
     <div className='w-full h-screen flex gap-y-5 flex-col items-center justify-center bg-green-700 text-5xl font-semibold'>
       <h1>Hello Nahid</h1>
       {
-        userdata ? userdata.map((elem) => {
+        userdata ? userdata.map((elem, index) => {
           return (
             <div className='flex flex-col items-start' key={elem._id}>
-              <h1>{elem.divisionName}</h1>
+              <h1>{index+'.' + elem.divisionName}</h1>
             </div>
           )
         }) : <p>Loading.....</p>
